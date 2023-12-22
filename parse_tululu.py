77d4d5edfb46cb83757fcdbf606d8a1c98dd617c
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2023-12-22 23:37:14 +0300

    refactoring

--- parse_tululu.py
+++ parse_tululu.py
@@ -1,131 +1,170 @@
+import argparse
 import json
-import logging.config
-import os
-from time import sleep
-from urllib.parse import urljoin, urlsplit
+import logging
+import posixpath
+import urllib
+from pathlib import Path
 
+import pathvalidate
 import requests
+import urllib3
 from bs4 import BeautifulSoup
-from pathvalidate import sanitize_filename
 
-from config import logger_config
+logger = logging.getLogger('parse_lululu')
 
-logger = logging.getLogger("parse_tululu_logger")
-
-
-def check_for_redirect(response):
-    if response.history:
-        raise requests.HTTPError
-
-
-def download_txt(book_id, filename, folder, timeout=10):
-    url = "https://tululu.org/txt.php"
-    payload = {'id': book_id}
-
-    response = requests.get(url, params=payload, timeout=timeout)
-    response.raise_for_status()
-
-    check_for_redirect(response)
-
-    book = response.text
-
-    os.makedirs(folder, exist_ok=True)
-
-    filename = ". ".join([str(book_id), sanitize_filename(filename)])
-    book_path = os.path.join(folder, ".".join([filename, 'txt']))
-
-    with open(book_path, 'w') as file:
-        file.write(book)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+BOOK_CATEGORY = 'https://tululu.org/l55/'
+VHOST = 'https://tululu.org'
+BOOK_DOWNLOAD_PATTERN = 'https://tululu.org/txt.php'
+BOOK_PAGE_PATTERN = 'https://tululu.org/b'
+IMAGES_FOLDER = 'images'
+BOOKS_FOLDER = 'books'
 
 
-def download_image(url, folder, timeout=10):
-    response = requests.get(url, timeout=timeout)
-    response.raise_for_status()
+def main():
+    logging.basicConfig(level=logging.INFO)
+    catalogue = []
+    last_page = get_last_category_page(BOOK_CATEGORY)
 
-    check_for_redirect(response)
-
-    _, image_filename = os.path.split(urlsplit(url).path)
-
-    os.makedirs(folder, exist_ok=True)
-
-    image_path = os.path.join(folder, image_filename)
-
-    with open(image_path, 'wb') as file:
-        file.write(response.content)
-
-
-def parse_book_page(response):
-    soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.select_one('h1')
-
-    book_name = title_tag.text.split('::')[0].strip()
-    book_author = title_tag.text.split('::')[1].strip()
-
-    image_src = soup.select_one('.bookimage img')['src']
-    book_image_url = urljoin(response.url, image_src)
-
-    book_genres = [genre.text for genre in soup.select('span.d_book a')]
-
-    book_comments = [comment.text for comment in soup.select('.texts span')]
-
-    book_details = {
-        'name': book_name,
-        'author': book_author,
-        'image_url': book_image_url,
-        'image_src': os.path.join('images', image_src.split('/')[-1]),
-        'genres': book_genres,
-        'comments': book_comments,
-    }
-
-    return book_details
-
-
-def get_books_by_ids(ids, dest_folder, skip_imgs=False, skip_txt=False):
-    logging.config.dictConfig(logger_config)
-    logger.debug('Start parsing books')
-
-    current_book_id_index = 0
-    timeout = 10
-
-    books_details = []
-
-    while current_book_id_index < len(ids):
-        current_book_id = ids[current_book_id_index]
-        logger.debug(f'Start while, trу to download book with ID {current_book_id} (index {current_book_id_index})')
+    parser = argparse.ArgumentParser(
+        description='Этот скрипт скачает книги и изображения')
+    parser.add_argument('--start_page', type=int,
+                        default=1,
+                        help='Начальная страница', )
+    parser.add_argument('--end_page', type=int,
+                        default=last_page + 1,
+                        help='Страница, перед которой остановить парсинг', )
+    parser.add_argument('--dest_folder',
+                        default='',
+                        help='Путь к каталогу с результатами парсинга')
+    parser.add_argument('--skip_imgs', action='store_true',
+                        help='не скачивать картинки', )
+    parser.add_argument('--skip_txt', action='store_true',
+                        help='не скачивать книги', )
+    parser.add_argument('--json_path',
+                        default='',
+                        help='указать свой путь к *.json файлу с результатами', )
+    args = parser.parse_args()
+    logger.info(args)
+    for page_number in range(args.start_page, args.end_page):
+        book_category_paginated = urllib.parse.urljoin(BOOK_CATEGORY,
+                                                       str(page_number))
+        response = requests.get(book_category_paginated, verify=False)
         try:
-            url = f'https://tululu.org/b{current_book_id}/'
-            book_page = requests.get(url, timeout=timeout)
-            book_page.raise_for_status()
+            response.raise_for_status()
+            logger.info(f'Начинаю парсить страницу = {page_number}')
+            pars_books_from_page(response,
+                                 args.dest_folder,
+                                 args.skip_imgs,
+                                 args.skip_txt,
+                                 catalogue
+                                 )
+        except requests.exceptions.HTTPError as err:
+            logger.warning(err)
+    write_books_meta_to_json(catalogue, args.dest_folder, args.json_path)
 
-            check_for_redirect(book_page)
 
-            book_details = parse_book_page(book_page)
-            if not skip_txt:
-                books_path = os.path.join(dest_folder, 'books')
-                download_txt(current_book_id, book_details['name'], books_path, timeout)
-            if not skip_imgs:
-                images_path = os.path.join(dest_folder, 'images')
-                download_image(book_details['image_url'], images_path, timeout)
-            books_details.append(book_details)
-            logger.debug('Book was downloaded')
-        except requests.exceptions.HTTPError:
-            logger.error(f'Book with ID {current_book_id} is not found')
-            current_book_id_index += 1
-        except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-            logger.error('Connection error')
-            sleep(5)
-        else:
-            current_book_id_index += 1
-            print('Название:', book_details['name'])
-            print('Автор:', book_details['author'])
-            print('')
+def write_books_meta_to_json(books_meta_raw, base_save_path, json_path):
+    full_path = posixpath.join(base_save_path, json_path, '')
+    Path(full_path).mkdir(parents=True, exist_ok=True)
+    with open(f'{full_path}books.json', 'w', encoding='UTF-8') as json_file:
+        json.dump(books_meta_raw, json_file, ensure_ascii=False, indent=2)
 
-    os.makedirs(dest_folder, exist_ok=True)
-    json_file_path = os.path.join(dest_folder, 'books_details.json')
 
-    with open(json_file_path, 'w', encoding='utf-8') as file:
-        json.dump(books_details, file, indent=4, ensure_ascii=False)
+def get_last_category_page(category_url):
+    response = requests.get(category_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    selector = '.center a:last-of-type'
+    last_page = soup.select_one(selector).text
+    return int(last_page)
+
+
+def parse_book_page(book_id):
+    book_url = f'{BOOK_PAGE_PATTERN}{book_id}'
+    response = requests.get(book_url, verify=False)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    h1_text = soup.select_one('body h1').text
+    title, author = h1_text.split('::')
+    pic_url = soup.select_one('.bookimage img')['src']
+    comments = [comment.text for comment in soup.select('.ow_px_td .black')]
+    genres = [genre.text for genre in soup.select('.ow_px_td span.d_book a')]
+    book_meta_info = {
+        'title': pathvalidate.sanitize_filename(title.strip()),
+        'author': author.strip(),
+        'img_src': '',
+        'book_path': '',
+        'comments': comments,
+        'genres': genres
+    }
+    return book_meta_info, pic_url
+
+
+def pars_books_from_page(response, base_save_path, skip_imgs, skip_txt,
+                         catalogue):
+    soup = BeautifulSoup(response.text, features='lxml')
+    selector = '.ow_px_td .bookimage a'
+    books_listing_raw = soup.select(selector)
+    for book_tag in books_listing_raw:
+        book_id = book_tag['href'].strip('/b')
+        book_meta_info, pic_url = parse_book_page(book_id)
+        book_path = download_txt(book_id,
+                                 base_save_path,
+                                 skip_txt,
+                                 book_meta_info["title"]
+                                 )
+        book_meta_info['book_path'] = book_path
+        if book_path:
+            img_src = download_image(pic_url,
+                                     base_save_path,
+                                     skip_imgs
+                                     )
+            book_meta_info['img_src'] = img_src
+            catalogue.append(book_meta_info)
+
+
+def download_txt(book_id, base_save_path, skip_txt, book_title):
+    """Функция для скачивания текстовых файлов.
+
+    Args:
+        book_id (int): Ссылка на id книги, которую хочется скачать.
+        # book_meta_info (dict): Словарь метаданных о книге.
+        base_save_path (str): Путь к каталогу с папкой books
+        skip_txt (bool): Если True, то пропустить скачивание текста
+        book_title(str): Название книги
+    Returns:
+        None
+    """
+    payload = {'id': book_id}
+    response = requests.get(BOOK_DOWNLOAD_PATTERN, params=payload, verify=False)
+    response.raise_for_status()
+    if not response.url == 'https://tululu.org/' and not skip_txt:
+        txt_full_path = posixpath.join(base_save_path, BOOKS_FOLDER, '')
+        Path(txt_full_path).mkdir(parents=True, exist_ok=True)
+        filename = f'{txt_full_path}{book_title}.txt'
+        with open(filename, 'w', encoding='UTF-8') as book:
+            book.write(response.text)
+        book_path = posixpath.join(txt_full_path,
+                                   f'{book_title}.txt')
+        # book_meta_info['book_path'] = book_path
+        logger.info(f'Книга скачана id={book_id}')
+        return book_path
+    logger.warning(f'Ошибка скачивания книги id={book_id}')
+
+
+def download_image(img_relative_src, base_save_path, skip_imgs):
+    if not skip_imgs:
+        pic_absolute_url = urllib.parse.urljoin(VHOST, img_relative_src)
+        response = requests.get(pic_absolute_url, verify=False)
+        response.raise_for_status()
+        image_full_path = posixpath.join(base_save_path, IMAGES_FOLDER, '')
+        Path(image_full_path).mkdir(parents=True, exist_ok=True)
+        img_name = posixpath.basename(pic_absolute_url)
+        with open(f'{image_full_path}{img_name}', 'wb') as img:
+            img.write(response.content)
+        img_src = posixpath.join(image_full_path, img_name)
+        return img_src
 
 
 if __name__ == '__main__':
-    get_books_by_ids([1, 2, 3], 'tululu_books')
+    main()
